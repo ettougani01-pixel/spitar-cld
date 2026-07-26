253bf888b45a95239230b9fc71c429e1fc7aa168
SPITAR <ettougani01@gmail.com>
2026-07-26 18:22:02 +0300

    Add assetlinks.json for TWA and fix start_url to landing page

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
         display: "standalone",
         orientation: "portrait",
         scope: "/",
-        start_url: "/dashboard",
+        start_url: "/",
         icons: [
           {
             src: "/icons/icon-192.png",
